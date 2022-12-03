4f443908ea239f7f8bd8d72dfc08c11abbaa4e11
John Davis <512jay@gmail.com>
2022-12-02 22:37:05 -0500

    Add ability to get the lowest flight price for a particular destination

--- flight_search.py
+++ flight_search.py
@@ -1,5 +1,6 @@
 import os
 import requests
+from datetime import datetime, timedelta
 
 
 class FlightSearch:
@@ -8,23 +9,36 @@ class FlightSearch:
         self.tequila_api = os.environ.get("TEQUILA_API")
         self.tequila_header = dict(apikey=self.tequila_api, accept="application/json")
         self.tequila_url = "https://api.tequila.kiwi.com/v2/search"
-        self.simple_search()
 
-    def simple_search(self):
+    def search(self, fly_to, fly_from="WAS"):
+        """Gets the cheapest price for the next 6 months"""
+        today = datetime.now()
+        date_from = today.strftime("%d/%m/%Y")
+        date_to = (today + timedelta(days=180)).strftime("%d/%m/%Y")
         payload = {
-            "fly_from": "LGA",
-            "fly_to": "MIA",
-            "dateFrom": "02/12/2022",
-            "dateTo": "02/05/2023",
+            "fly_from": fly_from,
+            "fly_to": fly_to,
+            "dateFrom": date_from,
+            "dateTo": date_to,
             "curr": "USD",
             "locale": "en",
-            "price_to": 150
         }
         response = requests.get(url=self.tequila_url, headers=self.tequila_header, params=payload)
         response.raise_for_status()
         flights = response.json()["data"]
-        for flight in flights:
-            print(flight["price"])
+        if len(flights) > 0:
+            cheapest_flight = flights[0]
+            lowest_price = cheapest_flight["price"]
+            for flight in flights:
+                if flight["price"] < lowest_price:
+                    lowest_price = flight["price"]
+                    cheapest_flight = flight
+            print(cheapest_flight["price"])
+            return cheapest_flight
+        return 9999999
 
 
-search = FlightSearch()
+flight = FlightSearch()
+print(f"{flight.search('LON') = }")
+print(f"{flight.search('PAR') = }")
+print(f"{flight.search('HOU') = }")
